85d39b90bf257689f7fcd952b67e9ba4a35c9199
taehuni <alsxogns72@gmail.com>
2025-12-15 17:23:33 +0900

    프론트: 카드에 설명/태그 조건부 표시 기능 추가

--- src/components/cards/PlaceListCard/PlaceListCard.jsx
+++ src/components/cards/PlaceListCard/PlaceListCard.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './PlaceListCard.css';
 
 export default function PlaceListCard({ data, onClick }){
-  
+
+  // 설명 우선, 없으면 태그, 그것도 없으면 주소 표시
+  const getDescriptionText = () => {
+    if (data.description) {
+      return data.description;
+    }
+    if (data.tags && Array.isArray(data.tags) && data.tags.length > 0) {
+      return data.tags.join(', ');
+    }
+    return data.address;
+  };
+
     return (
       <div className="card-body" onClick={onClick}>
         <div className="card-img-wrapper">
@@ -16,7 +27,7 @@ export default function PlaceListCard({ data, onClick }){
               <div className="rating-score">{data.rating}</div>
             </div>
           </div>
-          <div className="card-description">{data.descrption}</div>
+          <div className="card-description">{getDescriptionText()}</div>
           <div className="card-footer"></div>
         </div>
       </div>
